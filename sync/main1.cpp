906c13cffc3b8c97ed0efefe42b58759fad507ca
wai <johnchunwai@gmail.com>
2019-07-12 02:56:04 -0700

    sequencer first check in. screwed up all sound

--- sync/main1.cpp
+++ sync/main1.cpp
@@ -1,6 +1,7 @@
 #include "olcNoiseMaker.h"
 #include <iostream>
 #include <algorithm>
+#include <chrono>
 
 using namespace std;
 
@@ -26,7 +27,12 @@ namespace synth
         ftype on = 0.0;                 // time when note is on
         ftype off = 0.0;                // time when note is off
         bool active = false;            // when false, this note will be removed from the vector to make sound
-        instrument* channel = nullptr;  // each channel is handled by an instrument
+        instrument* instru = nullptr;   // the instrument for this note
+
+        note(int id, ftype on, ftype off, bool active, instrument *instru)
+            : id{ id }, on{ on }, off{ off }, active{ active }, instru{ instru }
+        {
+        }
     };
 
     // scale to freq conversion
@@ -44,7 +50,7 @@ namespace synth
 
     // osc -> oscillate
     // lfo -> low frequency oscillator for modulating the frequency of the wave
-    ftype osc(ftype time, ftype hertz, osc_t type, ftype lfo_hertz = 0.0, ftype lfo_amp = 0.0)
+    ftype osc(ftype time, ftype hertz, osc_t type, ftype lfo_hertz = 0.0, ftype lfo_amp = 0.0, ftype custom = 50.0)
     {
         auto freq = w(hertz) * time + lfo_amp * hertz * sin(w(lfo_hertz) * time);
         switch (type) {
@@ -61,7 +67,7 @@ namespace synth
             // adding different frequencies of sine waves creates the saw wave
             // supposed to be warmer?
             auto output = 0.0;
-            for (auto n = 1.0; n < 20.0; ++n) {
+            for (auto n = 1.0; n < custom; ++n) {
                 output += sin(n * freq) / n;
             }
             return output * 2.0 / PI;
@@ -147,13 +153,17 @@ namespace synth
 
         ftype vol;
         envelope_adsr env;
+        ftype max_life;
+        wstring name;
 
-        instrument(ftype v, ftype attk_time, ftype decay_time, ftype release_time, ftype attk_amp, ftype sustain_amp)
+        instrument(ftype v, ftype attk_time, ftype decay_time, ftype release_time, ftype attk_amp, ftype sustain_amp, ftype max_life, wstring_view name)
             : vol{ v }
             , env{ attk_time, decay_time, release_time, attk_amp, sustain_amp }
+            , max_life{max_life}
+            , name(name)
         {
         }
-        virtual ~instrument() {}
+        virtual ~instrument() = default;
 
         instrument(const instrument&) = delete;
         instrument& operator=(const instrument&) = delete;
@@ -164,11 +174,9 @@ namespace synth
     struct bell : public instrument
     {
         bell()
-            : instrument(1.0, 0.01, 1.0, 1.0, 1.0, 0.0)
+            : instrument(1.0, 0.01, 1.0, 1.0, 1.0, 0.0, 0.0, L"Bell")
         {
         }
-
-        virtual ~bell() {}
 
         virtual sound_ret_t sound(ftype time, note n) const override
         {
@@ -187,11 +195,9 @@ namespace synth
     struct bell8 : public instrument
     {
         bell8()
-            : instrument(1.0, 0.01, 1.0, 1.0, 1.0, 0.8)
+            : instrument(1.0, 0.01, 0.5, 1.0, 1.0, 0.8, 0.0, L"8-bit Bell")
         {
         }
-
-        virtual ~bell8() {}
 
         virtual sound_ret_t sound(ftype time, note n) const override
         {
@@ -200,9 +206,9 @@ namespace synth
             if (amp < g_epsilon) {
                 note_finished = true;
             }
-            auto sound = 1.0 * osc(time, scale(n.id + 12), osc_t::sine, 5.0, 0.001)
-                + 0.5 * osc(time, scale(n.id + 24), osc_t::sine)
-                + 0.25 * osc(time, scale(n.id + 36), osc_t::sine);
+            auto sound = 1.0 * osc(time - n.on, scale(n.id), osc_t::square, 5.0, 0.001)
+                + 0.5 * osc(time - n.on, scale(n.id + 12), osc_t::sine)
+                + 0.25 * osc(time - n.on, scale(n.id + 24), osc_t::sine);
             return { amp * sound * vol, note_finished };
         }
     };
@@ -210,11 +216,9 @@ namespace synth
     struct harmonica : public instrument
     {
         harmonica()
-            : instrument(0.5, 0.0, 0.0, 0.0, 1.0, 0.8)
+            : instrument(0.3, 0.0, 1.0, 0.1, 1.0, 0.95, 0.0, L"Harmonica")
         {
         }
-
-        virtual ~harmonica() {}
 
         virtual sound_ret_t sound(ftype time, note n) const override
         {
@@ -223,60 +227,159 @@ namespace synth
             if (amp < g_epsilon) {
                 note_finished = true;
             }
-            auto sound = 1.0 * osc(time, scale(n.id), osc_t::square, 5.0, 0.001)
-                + 0.5 * osc(time, scale(n.id + 12), osc_t::square)
-                + 0.05 * osc(time, scale(n.id + 24), osc_t::noise);
+            auto sound = 1.0 * osc(n.on - time, scale(n.id - 12), osc_t::saw_analog, 5.0, 0.001, 100)
+                + 1.0 * osc(time - n.on, scale(n.id), osc_t::square, 5.0, 0.001)
+                + 0.5 * osc(time - n.on, scale(n.id + 12), osc_t::square)
+                + 0.05 * osc(time - n.on, 0.0, osc_t::noise);
             return  { amp * sound * vol, note_finished };
         }
     };
 
-    //struct test1 : public instrument
-    //{
-    //    test1()
-    //    {
-    //        env = { 0.0,  0.0, 0.0, 0.8, 1.0 };
-    //    }
+    struct drumkick : public instrument
+    {
+        drumkick()
+            : instrument(1.0, 0.01, 0.15, 0.0, 1.0, 0.0, 1.5, L"Drum Kick")
+        {
+        }
 
-    //    ftype sound(ftype time, ftype freq) override
-    //    {
-    //        auto output = env.amplitude(time) * (
-    //            osc(freq, time, osc_t::saw_analog, 2.0, 0.1)
-    //            );
-    //        return output;
-    //    }
-    //};
+        virtual sound_ret_t sound(ftype time, note n) const override
+        {
+            bool note_finished = false;
+            auto amp = env.amplitude(time, n.on, n.off);
+            if (max_life > 0.0 && time - n.on >= max_life) {
+                note_finished = true;
+            }
+            auto sound = 0.99 * osc(time - n.on, scale(n.id - 36), osc_t::sine, 1.0, 1.0)
+                + 0.01 * osc(time - n.on, 0.0, osc_t::noise);
+            return  { amp * sound * vol, note_finished };
+        }
+    };
 
-    //struct test2 : public instrument
-    //{
-    //    test2()
-    //    {
-    //        env = { 0.1, 0.01, 0.2, 0.8, 1.0 };
-    //    }
+    struct drumsnare : public instrument
+    {
+        drumsnare()
+            : instrument(1.0, 0.0, 0.2, 0.0, 1.0, 0.0, 1.0, L"Drum Snare")
+        {
+        }
 
-    //    ftype sound(ftype time, ftype freq) override
-    //    {
-    //        auto output = env.amplitude(time) * (
-    //            osc(freq, time, osc_t::saw_digital, 2.0, 0.1)
-    //            );
-    //        return output;
-    //    }
-    //};
+        virtual sound_ret_t sound(ftype time, note n) const override
+        {
+            bool note_finished = false;
+            auto amp = env.amplitude(time, n.on, n.off);
+            if (max_life > 0.0 && time - n.on >= max_life) {
+                note_finished = true;
+            }
+            auto sound = 0.5 * osc(time - n.on, scale(n.id - 24), osc_t::sine, 0.5, 1.0)
+                + 0.5 * osc(time - n.on, 0.0, osc_t::noise);
+            return  { amp * sound * vol, note_finished };
+        }
+    };
+
+    struct drumhihat : public instrument
+    {
+        drumhihat()
+            : instrument(1.0, 0.01, 0.05, 0.0, 1.0, 0.0, 1.0, L"Drum HiHat")
+        {
+        }
+
+        virtual sound_ret_t sound(ftype time, note n) const override
+        {
+            bool note_finished = false;
+            auto amp = env.amplitude(time, n.on, n.off);
+            if (max_life > 0.0 && time - n.on >= max_life) {
+                note_finished = true;
+            }
+            auto sound = 0.1 * osc(time - n.on, scale(n.id - 12), osc_t::square, 1.5, 1.0)
+                + 0.9 * osc(time - n.on, 0.0, osc_t::noise);
+            return  { amp * sound * vol, note_finished };
+        }
+    };
+
+    struct sequencer
+    {
+        struct channel
+        {
+            instrument* instru = nullptr;
+            wstring beat_pattern = L""s;
+
+            channel(instrument* instru, wstring_view beat_pattern)
+                : instru(instru), beat_pattern(beat_pattern)
+            {
+            }
+        };
+
+        int beats;
+        int subbeats;
+        ftype tempo;
+        ftype beat_time;
+        ftype accum_time;
+        int curr_beat;
+        int total_beats;
+
+        vector<channel> channel_vec;
+        vector<note> note_vec;
+
+        sequencer(ftype tempo = 120.0f, int beats = 4, int subbeats = 4)
+            : beats{ beats }
+            , subbeats{ subbeats }
+            , tempo{ tempo }
+            , beat_time { 60.0 / tempo / (ftype) subbeats}
+            , accum_time { 0.0 }
+            , curr_beat{ 0 }
+            , total_beats{ subbeats * beats }
+            , channel_vec{}
+            , note_vec{}
+        {
+        }
+
+        void add_instrument(instrument* instru, wstring_view beat_pattern)
+        {
+            channel_vec.emplace_back(instru, beat_pattern);
+        }
+
+        int update(ftype elapsed)
+        {
+            note_vec.clear();
+
+            accum_time += elapsed;
+            while (accum_time >= beat_time) {
+                accum_time -= beat_time;
+
+                for (auto ch : channel_vec) {
+                    if (ch.beat_pattern[curr_beat] == L'X') {
+                        //int id = 0;                     // position in scale, id 0 is the note with g_octave_base_freq (A3)
+                        //ftype on = 0.0;                 // time when note is on
+                        //ftype off = 0.0;                // time when note is off
+                        //bool active = false;            // when false, this note will be removed from the vector to make sound
+                        //instrument* instru = nullptr;   // the instrument for this note
+                        note_vec.emplace_back(64, 0.0, 0.0, true, ch.instru);
+                    }
+                }
+
+                curr_beat = (curr_beat + 1) % total_beats;
+            }
+            return note_vec.size();
+        }
+    };
 }
 
 
-static auto init_channel_vec()
+static auto init_instrument_vec()
 {
-    vector<unique_ptr<synth::instrument>> channels;
-    channels.emplace_back(make_unique<synth::bell>());
-    channels.emplace_back(make_unique<synth::bell8>());
-    channels.emplace_back(make_unique<synth::harmonica>());
-    return channels;
+    vector<unique_ptr<synth::instrument>> instru_vec;
+    instru_vec.push_back(make_unique<synth::bell>());
+    instru_vec.push_back(make_unique<synth::bell8>());
+    instru_vec.push_back(make_unique<synth::harmonica>());
+    return instru_vec;
 }
 
 vector<synth::note> g_note_list;
 mutex g_notes_mutex;
-vector<unique_ptr<synth::instrument>> g_channels{ init_channel_vec() };
-atomic<synth::instrument*> g_active_channel = g_channels[0].get();
+vector<unique_ptr<synth::instrument>> g_instruments{ init_instrument_vec() };
+atomic<synth::instrument*> g_active_instrument = g_instruments[0].get();
+synth::drumkick instru_kick;
+synth::drumsnare instru_snare;
+synth::drumhihat instru_hihat;
 //shared_ptr<instrument> g_instrument = make_shared<harmonica>();
 
 template<typename V, typename Func>
@@ -297,7 +400,7 @@ ftype sound_func(int channel, ftype time)
     ftype mixed_sound = 0.0;
     scoped_lock lock(g_notes_mutex);
     for (auto& n : g_note_list) {
-        auto [sound, note_finished] = n.channel->sound(time, n);
+        auto [sound, note_finished] = n.instru->sound(time, n);
         mixed_sound += sound;
         if (note_finished && n.off > n.on) {
             n.active = false;
@@ -331,16 +434,26 @@ int main()
     olcNoiseMaker<int16_t> sound(devices[0], 44100, 1, 8, 512);
     sound.SetUserFunction(sound_func);
 
+    synth::sequencer seq(90.0);
+    seq.add_instrument(&instru_kick,  L"X...X...X..X.X..");
+    seq.add_instrument(&instru_snare, L"..X...X...X...X.");
+    seq.add_instrument(&instru_hihat, L"X.X.X.X.X.X.X.XX");
+
+    auto clock_prev_time = chrono::high_resolution_clock::now();
+    auto clock_curr_time = chrono::high_resolution_clock::now();
+    ftype elapsed = 0.0;
+    ftype wall_time = 0.0;
+
     constexpr string_view keys{ "AWSDRFTGHUJIKOL\xba\xdb\xde\xdd" };
-    constexpr string_view channel_keys{ "123" };
+    constexpr string_view instrument_keys{ "123" };
     //constexpr string_view osc_keys{ "123456" };
     //constexpr string_view env_keys{ "90" };
     int curr_key = -1;
-    int curr_channel_key = 0;
+    int curr_instrument_key = 0;
     //int curr_osc_key = -1;
     //int curr_env_key = -1;
     while (1) {
-        bool key_pressed = false;
+        //bool key_pressed = false;
 
         //for (int k = 0; k < env_keys.size(); k++) {
         //    if (GetAsyncKeyState((uint8_t)env_keys[k]) & 0x8000) {
@@ -363,18 +476,33 @@ int main()
         //    }
         //}
 
-        for (int k = 0; k < channel_keys.size(); k++) {
-            if (GetAsyncKeyState((uint8_t)channel_keys[k]) & 0x8000) {
-                if (curr_channel_key != k) {
-                    curr_channel_key = k;
-                    g_active_channel = g_channels[k].get();
+        for (int k = 0; k < instrument_keys.size(); k++) {
+            if (GetAsyncKeyState((uint8_t)instrument_keys[k]) & 0x8000) {
+                if (curr_instrument_key != k) {
+                    curr_instrument_key = k;
+                    g_active_instrument = g_instruments[k].get();
                 }
+            }
+        }
+
+        clock_curr_time = chrono::high_resolution_clock::now();
+        elapsed = chrono::duration<ftype>(clock_curr_time - clock_prev_time).count();
+        clock_prev_time = clock_curr_time;
+        wall_time += elapsed;
+        auto sound_time = sound.GetTime();
+
+        // update sequencer
+        int new_notes = seq.update(elapsed);
+        if (new_notes > 0) {
+            scoped_lock lock(g_notes_mutex);
+            for (auto n : seq.note_vec) {
+                n.on = sound_time;
+                g_note_list.push_back(n);
             }
         }
 
         for (int k = 0; k < keys.size(); k++) {
             auto key_state = GetAsyncKeyState((uint8_t)keys[k]);
-            auto now = sound.GetTime();
 
             // handle key press and release
             scoped_lock lock(g_notes_mutex);
@@ -385,7 +513,7 @@ int main()
                     // use >= instead of > to avoid the case when off==on and it does nothing
                     if (found_note->off >= found_note->on) {
                         // key was off but on again
-                        found_note->on = now;
+                        found_note->on = sound_time;
                     }
                     else {
                         // key was on and held, do nothing
@@ -393,7 +521,7 @@ int main()
                 }
                 else {
                     // key on and note not exist, create
-                    g_note_list.push_back({ k, now, 0.0, true, g_active_channel });
+                    g_note_list.emplace_back(k, sound_time, 0.0, true, g_active_instrument);
                 }
             }
             else {
@@ -402,7 +530,7 @@ int main()
                     // use >= instead of > to avoid the case when off==on and it does nothing
                     if (found_note->on >= found_note->off) {
                         // key was on, set to off
-                        found_note->off = now;
+                        found_note->off = sound_time;
                     }
                     else {
                         // key was off and stay off, do nothing
