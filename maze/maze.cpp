b74ceb5e6ec4d0af4cdb905b20ac2375f84bc883
John Chan <johnchan@ea.com>
2019-07-16 16:45:14 -0700

    [maze] better way to set cell width/height and have an appropriate size console window; add a wall at x=0 or y=0

--- maze/maze.cpp
+++ maze/maze.cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 namespace olc
 {
+    constexpr int g_maze_w = 40;
+    constexpr int g_maze_h = 25;
+    constexpr int g_cell_w = 3;
+    constexpr int pixel_offset_x = 1;
+    constexpr int pixel_offset_y = 1;
+    constexpr int g_window_w = g_maze_w * (g_cell_w + 1) + pixel_offset_x;	// cell true width + wall = g_cell_w + 1
+    constexpr int g_window_h = g_maze_h * (g_cell_w + 1) + pixel_offset_y;	// cell true height + wall = g_cell_w + 1
+
     class maze : public olc::cmd_engine
     {
     private:
@@ -33,10 +41,10 @@ namespace olc
 
     public:
         maze()
-            : _maze_w{ 30 }
-            , _maze_h{ 20 }
+            : _maze_w{ g_maze_w }
+            , _maze_h{ g_maze_h }
             , _maze(_maze_w * _maze_h, 0)
-            , _cell_w{ 3 }
+            , _cell_w{ g_cell_w }
             //, _visited_count{ 0 }
         {
             _app_name = L"Maze";
@@ -61,22 +69,22 @@ namespace olc
             // update maze
             //
             // CAUTION: don't capture by [*this], it will make a copy of this object and will invoke the destructor, killing the console buffer
-            auto index = [this](int x, int y) {
+            auto maze_index = [this](int x, int y) {
                 return y * _maze_w + x;
             };
             if (!_stack.empty()) {
                 vector<direction::enum_t> neighbours;
                 auto [curr_x, curr_y] = _stack.top();
-                if (curr_y > 0 && (_maze[index(curr_x, curr_y - 1)] & cell_attrib::visited) == 0) {
+                if (curr_y > 0 && (_maze[maze_index(curr_x, curr_y - 1)] & cell_attrib::visited) == 0) {
                     neighbours.push_back(direction::N);
                 }
-                if (curr_x < _maze_w - 1 && (_maze[index(curr_x + 1, curr_y)] & cell_attrib::visited) == 0) {
+                if (curr_x < _maze_w - 1 && (_maze[maze_index(curr_x + 1, curr_y)] & cell_attrib::visited) == 0) {
                     neighbours.push_back(direction::E);
                 }
-                if (curr_y < _maze_h - 1 && (_maze[index(curr_x, curr_y + 1)] & cell_attrib::visited) == 0) {
+                if (curr_y < _maze_h - 1 && (_maze[maze_index(curr_x, curr_y + 1)] & cell_attrib::visited) == 0) {
                     neighbours.push_back(direction::S);
                 }
-                if (curr_x > 0 && (_maze[index(curr_x - 1, curr_y)] & cell_attrib::visited) == 0) {
+                if (curr_x > 0 && (_maze[maze_index(curr_x - 1, curr_y)] & cell_attrib::visited) == 0) {
                     neighbours.push_back(direction::W);
                 }
 
@@ -86,23 +94,23 @@ namespace olc
                     // create path between the curr cell and neighbour
                     switch (next_dir) {
                     case direction::N:
-                        _maze[index(curr_x, curr_y)] |= cell_attrib::path_n;
-                        _maze[index(curr_x, curr_y - 1)] |= cell_attrib::path_s | cell_attrib::visited;
+                        _maze[maze_index(curr_x, curr_y)] |= cell_attrib::path_n;
+                        _maze[maze_index(curr_x, curr_y - 1)] |= cell_attrib::path_s | cell_attrib::visited;
                         _stack.emplace(curr_x, curr_y - 1);
                         break;
                     case direction::E:
-                        _maze[index(curr_x, curr_y)] |= cell_attrib::path_e;
-                        _maze[index(curr_x + 1, curr_y)] |= cell_attrib::path_w | cell_attrib::visited;
+                        _maze[maze_index(curr_x, curr_y)] |= cell_attrib::path_e;
+                        _maze[maze_index(curr_x + 1, curr_y)] |= cell_attrib::path_w | cell_attrib::visited;
                         _stack.emplace(curr_x + 1, curr_y);
                         break;
                     case direction::S:
-                        _maze[index(curr_x, curr_y)] |= cell_attrib::path_s;
-                        _maze[index(curr_x, curr_y + 1)] |= cell_attrib::path_n | cell_attrib::visited;
+                        _maze[maze_index(curr_x, curr_y)] |= cell_attrib::path_s;
+                        _maze[maze_index(curr_x, curr_y + 1)] |= cell_attrib::path_n | cell_attrib::visited;
                         _stack.emplace(curr_x, curr_y + 1);
                         break;
                     case direction::W:
-                        _maze[index(curr_x, curr_y)] |= cell_attrib::path_w;
-                        _maze[index(curr_x - 1, curr_y)] |= cell_attrib::path_e | cell_attrib::visited;
+                        _maze[maze_index(curr_x, curr_y)] |= cell_attrib::path_w;
+                        _maze[maze_index(curr_x - 1, curr_y)] |= cell_attrib::path_e | cell_attrib::visited;
                         _stack.emplace(curr_x - 1, curr_y);
                         break;
                     }
@@ -125,23 +133,23 @@ namespace olc
             for (int x = 0; x < _maze_w; ++x) {
                 for (int y = 0; y < _maze_h; ++y) {
                     // current cell = red, visited = white, non visited = blue
-                    int cell = _maze[index(x, y)];
+                    int cell = _maze[maze_index(x, y)];
                     short cell_color{ color_t::red };
                     if (x != curr_x || y != curr_y) {
                         cell_color = (cell & cell_attrib::visited) ? color_t::white : color_t::blue;
                     }
                     for (int px = 0; px < _cell_w; ++px) {
                         for (int py = 0; py < _cell_w; ++py) {
-                            draw(x * cell_plus_wall_w + px, y * cell_plus_wall_w + py, pixel_type::solid, cell_color);
+                            draw(x * cell_plus_wall_w + px + pixel_offset_x, y * cell_plus_wall_w + py + pixel_offset_y, pixel_type::solid, cell_color);
                         }
                     }
                     // draw paths, if there is a path, fill in white at the wall pixel to "break" the wall
                     for (int p = 0; p < _cell_w; ++p) {
                         if (cell & cell_attrib::path_s) {
-                            draw(x * cell_plus_wall_w + p, y * cell_plus_wall_w + _cell_w, pixel_type::solid, color_t::white);
+                            draw(x * cell_plus_wall_w + p + pixel_offset_x, y * cell_plus_wall_w + _cell_w + pixel_offset_y, pixel_type::solid, color_t::white);
                         }
                         if (cell & cell_attrib::path_e) {
-                            draw(x * cell_plus_wall_w + _cell_w, y * cell_plus_wall_w + p, pixel_type::solid, color_t::white);
+                            draw(x * cell_plus_wall_w + _cell_w + pixel_offset_x, y * cell_plus_wall_w + p + pixel_offset_y, pixel_type::solid, color_t::white);
                         }
                     }
                 }
@@ -163,11 +171,11 @@ int main()
 {
     try {
         olc::maze maze{};
-        maze.construct_console(120, 80, 8, 8);
+        maze.construct_console(olc::g_window_w, olc::g_window_h, 8, 8);
         maze.start();
     }
     catch (olc::olc_exception& e) {
-        wcerr << e.msg() << endl;
+        wcerr << e.msg().data() << endl;
     }
     return 0;
 }
