4aef16fd5a78bbdd77b8c51b7c37a19739acf198
wai <johnchunwai@gmail.com>
2019-07-18 01:27:07 -0700

    add solver using the same stack backtracking algorithm

--- maze/maze.cpp
+++ maze/maze.cpp
@@ -28,6 +28,7 @@ namespace olc
                 path_s = 0x04,
                 path_w = 0x08,
                 visited = 0x10,
+                solved = 0x20,
             };
         };
 
@@ -43,9 +44,9 @@ namespace olc
         maze()
             : _maze_w{ g_maze_w }
             , _maze_h{ g_maze_h }
-            , _maze(_maze_w * _maze_h, 0)
             , _cell_w{ g_cell_w }
-            //, _visited_count{ 0 }
+            , _maze(_maze_w * _maze_h, 0)
+            , _solve_maze(_maze_w* _maze_h, 0)
         {
             _app_name = L"Maze";
         }
@@ -56,14 +57,15 @@ namespace olc
             srand(time(nullptr));
             _stack.emplace(0, 0);
             _maze[0] = cell_attrib::visited;
-            //++_visited_count;
+            _solve_stack.emplace(0, 0);
+            _solve_maze[0] = cell_attrib::visited | cell_attrib::solved;
             return true;
         }
 
         virtual bool on_user_update(float elapsed) override
         {
             // slow down for animation
-            this_thread::sleep_for(5ms);
+            //this_thread::sleep_for(2ms);
 
             //
             // update maze
@@ -120,6 +122,84 @@ namespace olc
                     _stack.pop();
                 }
             }
+            else if (!_solve_stack.empty()) {
+                // solving
+                auto has_path = [this, &maze_index](int x, int y, direction::enum_t dir) {
+                    cell_attrib::enum_t dir_val;
+                    switch (dir) {
+                    case direction::N:
+                        dir_val = cell_attrib::path_n;
+                        break;
+                    case direction::E:
+                        dir_val = cell_attrib::path_e;
+                        break;
+                    case direction::S:
+                        dir_val = cell_attrib::path_s;
+                        break;
+                    case direction::W:
+                        dir_val = cell_attrib::path_w;
+                        break;
+                    }
+                    return (_maze[maze_index(x, y)] & dir_val) != 0;
+                };
+                vector<direction::enum_t> neighbours;
+                auto [curr_x, curr_y] = _solve_stack.top();
+                if (curr_y > 0 && (_solve_maze[maze_index(curr_x, curr_y - 1)] & cell_attrib::visited) == 0 &&
+                    has_path(curr_x, curr_y, direction::N)) {
+                    neighbours.push_back(direction::N);
+                }
+                if (curr_x < _maze_w - 1 && (_solve_maze[maze_index(curr_x + 1, curr_y)] & cell_attrib::visited) == 0 &&
+                    has_path(curr_x, curr_y, direction::E)) {
+                    neighbours.push_back(direction::E);
+                }
+                if (curr_y < _maze_h - 1 && (_solve_maze[maze_index(curr_x, curr_y + 1)] & cell_attrib::visited) == 0 &&
+                    has_path(curr_x, curr_y, direction::S)) {
+                    neighbours.push_back(direction::S);
+                }
+                if (curr_x > 0 && (_solve_maze[maze_index(curr_x - 1, curr_y)] & cell_attrib::visited) == 0 &&
+                    has_path(curr_x, curr_y, direction::W)) {
+                    neighbours.push_back(direction::W);
+                }
+
+                if (!neighbours.empty()) {
+                    // move to next neighbour
+                    direction::enum_t next_dir = neighbours[rand() % neighbours.size()];
+                    // create path between the curr cell and neighbour
+                    switch (next_dir) {
+                    case direction::N:
+                        _solve_maze[maze_index(curr_x, curr_y)] |= cell_attrib::path_n;
+                        _solve_maze[maze_index(curr_x, curr_y - 1)] |= cell_attrib::path_s | cell_attrib::visited | cell_attrib::solved;
+                        _solve_stack.emplace(curr_x, curr_y - 1);
+                        break;
+                    case direction::E:
+                        _solve_maze[maze_index(curr_x, curr_y)] |= cell_attrib::path_e;
+                        _solve_maze[maze_index(curr_x + 1, curr_y)] |= cell_attrib::path_w | cell_attrib::visited | cell_attrib::solved;
+                        _solve_stack.emplace(curr_x + 1, curr_y);
+                        break;
+                    case direction::S:
+                        _solve_maze[maze_index(curr_x, curr_y)] |= cell_attrib::path_s;
+                        _solve_maze[maze_index(curr_x, curr_y + 1)] |= cell_attrib::path_n | cell_attrib::visited | cell_attrib::solved;
+                        _solve_stack.emplace(curr_x, curr_y + 1);
+                        break;
+                    case direction::W:
+                        _solve_maze[maze_index(curr_x, curr_y)] |= cell_attrib::path_w;
+                        _solve_maze[maze_index(curr_x - 1, curr_y)] |= cell_attrib::path_e | cell_attrib::visited | cell_attrib::solved;
+                        _solve_stack.emplace(curr_x - 1, curr_y);
+                        break;
+                    }
+
+                    if (auto [dest_x, dest_y] = _solve_stack.top(); dest_x == _maze_w - 1 && dest_y == _maze_h - 1) {
+                        // reached the end, we solved the maze, empty the stack
+                        _solve_stack = {};
+                    }
+                }
+                else {
+                    // no more neighbour, pop
+                    // mark it as not solved
+                    _solve_stack.pop();
+                    _solve_maze[maze_index(curr_x, curr_y)] &= ~cell_attrib::solved;
+                }
+            }
 
             //
             // === rendering ===
@@ -132,24 +212,32 @@ namespace olc
             // draw cells
             for (int x = 0; x < _maze_w; ++x) {
                 for (int y = 0; y < _maze_h; ++y) {
-                    // current cell = red, visited = white, non visited = blue
+                    // current cell = red, visited = white, non visited = blue, solved = cyan
                     int cell = _maze[maze_index(x, y)];
+                    int solve_cell = _solve_maze[maze_index(x, y)];
                     short cell_color{ color_t::red };
                     if (x != curr_x || y != curr_y) {
-                        cell_color = (cell & cell_attrib::visited) ? color_t::white : color_t::blue;
+                        if (solve_cell & cell_attrib::solved) {
+                            cell_color = color_t::cyan;
+                        }
+                        else {
+                            cell_color = (cell & cell_attrib::visited) ? color_t::white : color_t::blue;
+                        }
                     }
                     for (int px = 0; px < _cell_w; ++px) {
                         for (int py = 0; py < _cell_w; ++py) {
                             draw(x * cell_plus_wall_w + px + pixel_offset_x, y * cell_plus_wall_w + py + pixel_offset_y, pixel_type::solid, cell_color);
                         }
                     }
-                    // draw paths, if there is a path, fill in white at the wall pixel to "break" the wall
+                    // draw paths, if there is a path, fill in white/cyan at the wall pixel to "break" the wall
                     for (int p = 0; p < _cell_w; ++p) {
                         if (cell & cell_attrib::path_s) {
-                            draw(x * cell_plus_wall_w + p + pixel_offset_x, y * cell_plus_wall_w + _cell_w + pixel_offset_y, pixel_type::solid, color_t::white);
+                            color_t::enum_t wall_color = (solve_cell & cell_attrib::path_s && _solve_maze[maze_index(x, y + 1)] & cell_attrib::solved) ? color_t::cyan : color_t::white;
+                            draw(x * cell_plus_wall_w + p + pixel_offset_x, y * cell_plus_wall_w + _cell_w + pixel_offset_y, pixel_type::solid, wall_color);
                         }
                         if (cell & cell_attrib::path_e) {
-                            draw(x * cell_plus_wall_w + _cell_w + pixel_offset_x, y * cell_plus_wall_w + p + pixel_offset_y, pixel_type::solid, color_t::white);
+                            color_t::enum_t wall_color = (solve_cell & cell_attrib::path_e && _solve_maze[maze_index(x + 1, y)] & cell_attrib::solved) ? color_t::cyan : color_t::white;
+                            draw(x * cell_plus_wall_w + _cell_w + pixel_offset_x, y * cell_plus_wall_w + p + pixel_offset_y, pixel_type::solid, wall_color);
                         }
                     }
                 }
@@ -163,7 +251,8 @@ namespace olc
         const int _cell_w;  // excluding wall of 1 pixel
         vector<int> _maze;
         stack<pair<int, int>> _stack;
-        //int _visited_count;
+        vector<int> _solve_maze;
+        stack<pair<int, int>> _solve_stack;
     };
 }
 
