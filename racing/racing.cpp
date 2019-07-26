c4c2c41b1449a95b83b94af71ce025d3ae01b7ed
wai <johnchunwai@gmail.com>
2019-07-26 00:47:28 -0700

    draw basic car and road with perspective

--- racing/racing.cpp
+++ racing/racing.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+constexpr auto g_screen_width = 160;
+constexpr auto g_screen_height = 100;
+constexpr auto g_car_y = 80;
+constexpr auto g_car_w = 14;
+
+
 namespace olc {
     class racing : public cmd_engine {
+    private:
+        float car_pos = 0.0f;
+
     public:
         // Inherited via cmd_engine
         virtual bool on_user_init() override
@@ -12,11 +21,15 @@ namespace olc {
         }
         virtual bool on_user_update(float elapsed) override
         {
+            // draw road
             for (auto y = 0; y < height() / 2; ++y) {
                 // bottom of the screen is for the road
                 for (auto x = 0; x < width(); ++x) {
+                    auto perspective = (float) y / (height() / 2.0f);
+
                     auto mid_pt = 0.5f;
-                    auto road_w = 0.6f;
+                    // 0.1f is min width at the top and 0.9f is the max width at the bottom
+                    auto road_w = 0.1f + perspective * 0.8f;
                     auto clip_w = road_w * 0.15f;
 
                     road_w *= 0.5f;
@@ -38,6 +51,18 @@ namespace olc {
                     }
                 }
             }
+
+            // draw car
+            int car_x = width() / 2 + static_cast<int>(car_pos * width() / 2.0f) - g_car_w / 2;
+            int car_y = g_car_y;
+            draw_string_alpha(car_x, car_y++, L"   ||####||   ");
+            draw_string_alpha(car_x, car_y++, L"      ##      ");
+            draw_string_alpha(car_x, car_y++, L"     ####     ");
+            draw_string_alpha(car_x, car_y++, L"     ####     ");
+            draw_string_alpha(car_x, car_y++, L"|||  ####  |||");
+            draw_string_alpha(car_x, car_y++, L"|||########|||");
+            draw_string_alpha(car_x, car_y++, L"|||  ####  |||");
+
 	        return true;
         }
     };
